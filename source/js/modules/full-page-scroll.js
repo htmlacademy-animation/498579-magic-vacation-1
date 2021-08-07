7ee09d62125ae977780cf93018b6521473cc187f
olesia-sev <olesya0191@gmail.com>
2021-08-07 23:47:22 +0300

    module3-task1: Добавляет SMIL анимацию глвного приза

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -8,6 +8,7 @@ export default class FullPageScroll {
     this.menuElements = document.querySelectorAll(`.page-header__menu .js-menu-link`);
 
     this.activeScreen = 0;
+    this.previousscreen = 0;
     this.onScrollHandler = this.onScroll.bind(this);
     this.onUrlHashChengedHandler = this.onUrlHashChanged.bind(this);
   }
@@ -29,9 +30,18 @@ export default class FullPageScroll {
 
   onUrlHashChanged() {
     const newIndex = Array.from(this.screenElements).findIndex((screen) => location.hash.slice(1) === screen.id);
+    this.previousScreen = this.activeScreen;
     this.activeScreen = (newIndex < 0) ? 0 : newIndex;
     this.changePageDisplay();
   }
+
+  activateSvg() {
+    if (this.activeScreen === 2 && this.previousScreen !== 2) {
+      const image = document.querySelector(`.js-prize-1`);
+      image.src = `img/module-3/img/primary-award.svg?${new Date().getTime()}`;
+    }
+  }
+
 
   changePageDisplay() {
     this.changeVisibilityDisplay();
@@ -40,6 +50,7 @@ export default class FullPageScroll {
   }
 
   changeVisibilityDisplay() {
+    this.activateSvg();
     const previousScreen = [...this.screenElements].find((el) => !el.classList.contains(`screen--hidden`));
     const currentScreen = this.screenElements[this.activeScreen];
     const overlaySlide = document.createElement(`div`);
